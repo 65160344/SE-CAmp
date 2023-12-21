6ccc09fb21ef19e343fbaa2f9723996e5318ab42
66160112 <152171701+65160344@users.noreply.github.com>
2023-12-21 19:35:00 +0700

    WS

--- Test.php
+++ Test.php
@@ -1,9 +1,60 @@
 <html>
-    <head>
-        <meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
 </head>
 <body>
-    <h1><?php echo "hello PHP"?>
-    <h1><?php echo "I'M COME BACK!!!!!!!"?>
+    <h1><?php echo "hello PHP<br><br>"?></h1>
+    <h1><?php echo "<h1>Hello PHP<brb></h1>"?>
+    <?php $var_a = "SECAMP" ?>
+    <?php echo $var_b = 'Hello var_a' ?>
+    <br>
+    <?php echo $var_c = "hello $var_a" ?>
+    <br>
+    <?php
+    $var_c = $var_a = 1;
+    
+    if($var_b === $var_c){
+        //true
+        echo '$var_b === $var_c';
+    }else if($var_b <> $var_c){
+        //true
+        echo '$var_b <> $var_c';
+    }else{
+        //false
+        echo 'else';
+    } ?>
+    <br>
+    <?php echo $var_c = 2; ?>
+    <br>
+    <?php echo $var_c;
+    switch($var_c){
+        case 1:
+            echo "1";
+            break;
+        default:
+            echo "test";
+    }
+    $arry = array(1,2,3);
+    $arry1 = []; //{}
+    $arry2[] = 1;
+    $arry2[] = 2;
+    $arry2[] = 3;
+    $arry2[] = "Hello";
+    $arry2['SE'] = "World";
+    ?>
+    <?php print_r($arry);?>
+    <pre><?php print_r($arry);?></pre>
+    <pre><?php print_r($arry1);?></pre>
+    <pre><?php print_r($arry2);?></pre>
+    <?php for($i =0; $i < count($arry2); $i++){?>
+    <h3><?php echo $arry2[$i]; ?></h3>
+    <?php
+    }?>
+
+    <?php foreach($arry2 as $key => $value){?>
+    <h3><?php echo $arry2[$key]; ?> : <?php echo $value ?></h3>
+    <?php
+    }?>
 </body>
-    </html>
+
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <html>
-
+    <head>
+        <meta charset="utf-8">
+    </head>
 <body>
-    <h1>This is PHP<?php echo 'Hello World'; ?></h1>
+    <p>
+        <?php$multiply_x = 2;
+        echo"<h1>ตารางสูตรคูณแม่ $multiply_x </h1>";?>
+            <div>
+                <?php
+                $i = 1;
+
+                while ($i <= 24){
+                    echo "$multiply_x X $i =";
+                    echo $i*$multiply_x;
+                    echo "br"
+                    $i++;
+                }?>
+            </div>
+    </p>
+    <style>
+        h1{
+            margin-top: 0px;
+            margin-bottom: 10px;
+            font-family: sans-serif;
+            background: linear-gradient(to right, #ef5350,#f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+    </style>
     
 </body>
 
