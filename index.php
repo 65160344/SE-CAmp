7e14c83f0da74ce8edaef509c82b08a13719887d
66160112 <152171701+65160344@users.noreply.github.com>
2023-12-25 19:04:59 +0700

    WS3

--- index.php
+++ index.php
@@ -1,34 +1,48 @@
 <html>
-    <head>
-        <meta charset="utf-8">
-    </head>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
 <body>
     <p>
-        <?php$multiply_x = 2;
-        echo"<h1>ตารางสูตรคูณแม่ $multiply_x </h1>";?>
-            <div>
-                <?php
-                $i = 1;
-
-                while ($i <= 24){
-                    echo "$multiply_x X $i =";
-                    echo $i*$multiply_x;
-                    echo "br"
-                    $i++;
-                }?>
-            </div>
+    <h1>
+        <?php 
+        $multiply_x = 2;
+        echo "ตารางแม่สตรคูณ $multiply_x ";
+        ?>
+    </h1>
+        <div>
+            <?php
+            for($i = 1; $i <= 24 ;$i++){
+                ?>
+                <?php echo "$multiply_x X $i =";?>
+                <?php echo $i*$multiply_x;?>
+                <?php echo "<br>";
+            }?>
+        </div>
     </p>
-    <style>
-        h1{
-            margin-top: 0px;
-            margin-bottom: 10px;
-            font-family: sans-serif;
-            background: linear-gradient(to right, #ef5350,#f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent;
-        }
-    </style>
-    
+        <style>
+            h1{
+                margin-top: 0px;
+                margin-bottom: 10px;
+                text-align: center;
+                background: linear-gradient(#e66465, #9198e5);
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+            }
+            div{
+                background: linear-gradient(#e66465, #9198e5);
+                background-color: light;
+                width: 200px;
+                height: 430px;
+                border: 1px solid black;
+                overflow-y: auto;
+                text-align: center;
+                margin: 0 auto;
+                margin-top: 50px;
+            }
+        </style>
 </body>
 
 </html>
